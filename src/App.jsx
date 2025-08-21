4361fc4529e1646ed3a9b8e9779c5b0685b1a3a6
Om Sandeep Salpekar <omsandeep.salpekar@o9solutions.com>
2025-08-21 05:31:04 +0530

    Initial commit: Setup project with chatbot UI and inspector

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import ListBoxPage from './pages/ListBoxPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/listbox" element={<ListBoxPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
